f945dbd16bbc5f1894a319dc697110a5e30aad46
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-03-26 22:09:33 +0300

    Add files via upload

---
+++ Lecture1_DataTypes/ArithmeticOperations/src/main/java/MathOperationsDemo.java
@@ -0,0 +1,18 @@
+public class MathOperationsDemo {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        int w = a+b;
+        int x = a-b;
+        int y = a*b;
+        float z = a/b;
+        System.out.println("Математические операции, где а=10, b=5");
+        System.out.println("Сложение: "+w);
+        System.out.println("Вычитание: "+x);
+        System.out.println("Умножение: "+y);
+        System.out.println("Деление: "+z);
+    }
+}
