ae96c3afed1aaa1f565b7ac383a415cb2ef07fc4
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-03-26 23:21:54 +0300

    Add files via upload

---
+++ Lecture1_DataTypes/Random Generator/src/main/java/RandomGeneratorDemo.java
@@ -0,0 +1,16 @@
+public class RandomGeneratorDemo {
+
+    public static void main(String[] args) {
+
+        double a = Math.random();
+        double b = Math.random();
+        double c = Math.random();
+
+        double x = a + b + c;
+
+        System.out.println("Первое сгенерированное число: "+a);
+        System.out.println("Первое сгенерированное число: "+b);
+        System.out.println("Первое сгенерированное число: "+c);
+        System.out.println("Сумма чисел: "+x);
+    }
+}
