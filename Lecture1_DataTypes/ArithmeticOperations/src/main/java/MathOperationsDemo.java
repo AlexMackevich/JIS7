979777730b01cfeffaaba133293881b3c5cb515b
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-03-29 21:44:28 +0300

    Update MathOperationsDemo.java

--- Lecture1_DataTypes/ArithmeticOperations/src/main/java/MathOperationsDemo.java
+++ Lecture1_DataTypes/ArithmeticOperations/src/main/java/MathOperationsDemo.java
@@ -5,14 +5,14 @@ public class MathOperationsDemo {
         int a = 10;
         int b = 5;
 //Разделяй пробелами выражения с арифметическими знаками включая конкатенацию. Тогда будет читабельней
-        int w = a+b;
-        int x = a-b;
-        int y = a*b;
-        float z = a/b;
-        System.out.println("Математические операции, где а=10, b=5");
-        System.out.println("Сложение: "+w);
-        System.out.println("Вычитание: "+x);
-        System.out.println("Умножение: "+y);
-        System.out.println("Деление: "+z);
+        int w = a + b;
+        int x = a - b;
+        int y = a * b;
+        float z = a / b;
+        System.out.println("Математические операции, где а = 10, b = 5");
+        System.out.println("Сложение: " + w);
+        System.out.println("Вычитание: " + x);
+        System.out.println("Умножение: " + y);
+        System.out.println("Деление: " + z);
     }
 }
