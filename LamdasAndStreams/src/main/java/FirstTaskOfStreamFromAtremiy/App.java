92f42641cf574342e6f8bae2838beb04252e0130
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-05-25 13:56:47 +0300

    Add files via upload

---
+++ LamdasAndStreams/src/main/java/FirstTaskOfStreamFromAtremiy/App.java
@@ -0,0 +1,21 @@
+package FirstTaskOfStreamFromAtremiy;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    CollectionOfRandomNumbers collectionOfRandomNumbers = new CollectionOfRandomNumbers();
+
+        List<Integer> collection = collectionOfRandomNumbers.setOfRandomNumbers();
+        System.out.println(collection);
+
+        List<Integer> collectionOfEvenNumbers = collection.stream()
+                .filter(it -> it % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(collectionOfEvenNumbers);
+    }
+}
---
+++ LamdasAndStreams/src/main/java/ThirdTaskOfStreamFromArtemiy/model/Price.java
@@ -0,0 +1,16 @@
+package ThirdTaskOfStreamFromArtemiy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Price {
+
+   private double priceOfProduct;
+   private double discountOnProduct;
+
+}
