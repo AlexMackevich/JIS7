ff0a0ac4dfb0d0a43677c751245d096fcc4398e3
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-05-27 08:13:35 +0300

    Task4With*

---
+++ LamdasAndStreams/src/main/java/ForthExampleFromArtemiy/Model/Person.java
@@ -0,0 +1,25 @@
+package ForthExampleFromArtemiy.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Person {
+
+    private String name;
+    private long levelOfStatus;
+    private List<Skill> skills;
+
+    public Person(String name, long levelOfStatus, Skill... skill) {
+        this.name = name;
+        this.levelOfStatus = levelOfStatus;
+        this.skills = Arrays.asList(skill);
+    }
+
+}
