71bd07f225737aff150ead30fed36324cf0cb2de
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-06-01 09:47:41 +0300

    updates from the review

--- LamdasAndStreams/src/main/java/FirstTaskOfStreamFromAtremiy/CollectionOfRandomNumbers.java
+++ LamdasAndStreams/src/main/java/FirstTaskOfStreamFromAtremiy/CollectionOfRandomNumbers.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class CollectionOfRandomNumbers {
 
-
     public List setOfRandomNumbers () {
 
         List<Integer> collectionOfRandomNumbers = new ArrayList<>();
