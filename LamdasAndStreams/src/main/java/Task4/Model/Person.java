ef8b0a383afe942c5bf3f879ff5421ec2b0f779e
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-06-09 12:41:41 +0300

    some updates

--- LamdasAndStreams/src/main/java/Task4/Model/Person.java
+++ LamdasAndStreams/src/main/java/Task4/Model/Person.java
@@ -3,21 +3,21 @@ package Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.Arrays;
 import java.util.List;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class Person {
-
     private String name;
-    private long levelOfStatus;
-    private List<Skill> skills;
+    private long lvlOfStatus;
+    private List<Skill> skill;
 
-    public Person(String name, long levelOfStatus, Skill... skill) {
+    public Person(String name, long lvlOfStatus, Skill... skill) {
         this.name = name;
-        this.levelOfStatus = levelOfStatus;
-        this.skills = Arrays.asList(skill);
+        this.lvlOfStatus = lvlOfStatus;
+        this.skill = Arrays.asList(skill);
     }
 }
