23ff35a85682882e7ad4715e0a45559ae1863a11
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-06-09 12:46:14 +0300

    Update PersonServiceImpl.java

--- LamdasAndStreams/src/main/java/Task4/Service/PersonServiceImpl.java
+++ LamdasAndStreams/src/main/java/Task4/Service/PersonServiceImpl.java
@@ -2,32 +2,32 @@ package Service;
 
 import Model.Person;
 import Model.Skill;
-import Repository.Repository;
+import Repository.PersonRepository;
+
 import java.util.Comparator;
-import java.util.List;
 import java.util.Objects;
+
 import static Repository.Repository.listOfPersons;
 
 public class PersonServiceImpl implements PersonService {
 
     static {
-        Repository repository = new Repository();
-        repository.addPersonsToListOfPersons();
+        PersonRepository.initPersonList();
     }
 
     @Override
-    public Person findPersonsWithNecessarySkillAndPercentage(String nameOfNecessarySkill) {
-        var necessarySkill = findNecessarySkill(listOfPersons, nameOfNecessarySkill);
-        return listOfPersons.stream()
-                .filter(it -> it.getSkills().contains(necessarySkill))
+    public Person findPersonWithNecessarySkill(String nameOfNecessarySkill) {
+        var necessarySkill = findNecessarySkill(nameOfNecessarySkill);
+        return personList.stream()
+                .filter(it -> it.getSkill().contains(necessarySkill))
                 .findFirst().orElseThrow();
     }
 
-    private Skill findNecessarySkill(List<Person> listOfPersons, String nameOfNecessarySkill){
-        return Repository.listOfPersons.stream()
-                .flatMap(it -> it.getSkills().stream())
+    private Skill findNecessarySkill(String nameOfNecessarySkill){
+        return personList.stream()
+                .flatMap(it -> it.getSkill().stream())
                 .filter(skill -> Objects.equals(skill.getNameOfSkill(), nameOfNecessarySkill))
-                .max(Comparator.comparingInt(Skill::getSkillPercentage))
-                .orElseThrow(IllegalArgumentException::new);
+                .max(Comparator.comparing(Skill::getSkillPercentage))
+                .orElseThrow(IllegalAccessError::new);
     }
 }
