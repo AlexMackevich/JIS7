8c77959eba404223241edf6256900ec33d05f837
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-06-09 12:47:30 +0300

    Update App.java

--- LamdasAndStreams/src/main/java/Task4/App.java
+++ LamdasAndStreams/src/main/java/Task4/App.java
@@ -3,11 +3,11 @@ import Service.PersonServiceImpl;
 public class App {
 
     public static void main(String[] args) {
-
         PersonServiceImpl personService = new PersonServiceImpl();
-        System.out.println(personService.findPersonsWithNecessarySkillAndPercentage("English"));
+
+        System.out.println(personService.findPersonWithNecessarySkill("English"));
         System.out.println("___________________");
-        System.out.println(personService.findPersonsWithNecessarySkillAndPercentage("Kannada"));
+        System.out.println(personService.findPersonWithNecessarySkill("Kannada"));
         System.out.println("___________________");
     }
 }
